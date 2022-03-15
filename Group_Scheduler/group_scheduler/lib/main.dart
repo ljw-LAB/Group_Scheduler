cc4b782329507e16db9abd15812272cddfc94d17
js <js@192.168.2.6>
2022-03-15 22:27:21 +0700

    Register user function created

--- Group_Scheduler/group_scheduler/lib/main.dart
+++ Group_Scheduler/group_scheduler/lib/main.dart
@@ -5,18 +5,23 @@ import 'package:flutter/material.dart';
 import 'package:group_scheduler/pages/login_page.dart';
 import 'package:group_scheduler/pages/register_page.dart';
 import 'package:group_scheduler/pages/home_page.dart';
+import 'package:group_scheduler/services/auth_service.dart';
 
 // 자체 제작 서비스들
 import 'package:group_scheduler/services/diary_service.dart';
 
 // 3rd Party Packages - 외부 패키지들
 import 'package:provider/provider.dart'; // 프로바이더
+import 'package:firebase_core/firebase_core.dart'; // 파이어베이스
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // main 함수에서 async 사용하기 위함
+  await Firebase.initializeApp();
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (context) => DiaryService()),
+        ChangeNotifierProvider(create: (context) => AuthService()),
       ],
       child: const MyApp(),
     ),
@@ -30,7 +35,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: LoginPage(),
     );
   }
 }
--- Group_Scheduler/group_scheduler/lib/pages/login_page.dart
+++ Group_Scheduler/group_scheduler/lib/pages/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:group_scheduler/services/auth_service.dart';
+import 'package:provider/provider.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -8,100 +10,128 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: SizedBox(
-          width: double.infinity,
-          child: Stack(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 24),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    /// Tip : 기기 높이의 %로 줘야 각 기기별로 적절한 위치에 배치할 수 있어요.
-                    SizedBox(height: MediaQuery.of(context).size.height * 0.15),
+    return Consumer<AuthService>(
+      builder: (context, authService, child) {
+        return Scaffold(
+          body: SafeArea(
+            child: SizedBox(
+              width: double.infinity,
+              child: Stack(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        /// Tip : 기기 높이의 %로 줘야 각 기기별로 적절한 위치에 배치할 수 있어요.
+                        SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.15),
 
-                    /// 예약내역
-                    Text(
-                      "Group Scheduler",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w900,
-                        fontSize: 32,
-                      ),
-                    ),
-                    SizedBox(height: 20),
-
-                    /// 이메일
-                    TextField(
-                      //controller: emailController,
-                      decoration: InputDecoration(hintText: "이메일"),
-                    ),
-                    SizedBox(height: 32),
-
-                    /// 비밀번호
-                    TextField(
-                      //controller: passwordController,
-                      obscureText: false, // 비밀번호 안보이게
-                      decoration: InputDecoration(hintText: "비밀번호"),
-                    ),
-                    SizedBox(height: 32),
-
-                    SizedBox(
-                      width: double.infinity, // <-- match_parent
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                            shape: MaterialStateProperty.all<
-                                RoundedRectangleBorder>(RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(18.0),
-                        ))),
-
-                        /// 로그인 버튼
-                        child: Text(
-                          "로그인",
+                        /// 예약내역
+                        Text(
+                          "Group Scheduler",
                           style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 20),
+                            fontWeight: FontWeight.w900,
+                            fontSize: 32,
+                          ),
                         ),
-                        onPressed: () {
-                          // 로그인 성공시 HomePage로 이동
-                          // Navigator.pushReplacement(
-                          //   context,
-                          //   MaterialPageRoute(builder: (_) => HomePage()),
-                          // );
-                        },
-                      ),
-                    ),
+                        SizedBox(height: 20),
 
-                    SizedBox(
-                      width: double.infinity, // <-- match_parent
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                            shape: MaterialStateProperty.all<
-                                RoundedRectangleBorder>(RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(18.0),
-                        ))),
-
-                        /// 회원가입 버튼
-                        child: Text(
-                          "회원가입",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 20),
+                        /// 이메일
+                        TextField(
+                          controller: emailController,
+                          decoration: InputDecoration(hintText: "이메일"),
                         ),
-                        onPressed: () {
-                          // 회원가입
-                          print("sign up");
-                        },
-                      ),
+                        SizedBox(height: 32),
+
+                        /// 비밀번호
+                        TextField(
+                          controller: passwordController,
+                          obscureText: false, // 비밀번호 안보이게
+                          decoration: InputDecoration(hintText: "비밀번호"),
+                        ),
+                        SizedBox(height: 32),
+
+                        SizedBox(
+                          width: double.infinity, // <-- match_parent
+                          child: ElevatedButton(
+                            style: ButtonStyle(
+                                shape: MaterialStateProperty.all<
+                                        RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                            ))),
+
+                            /// 로그인 버튼
+                            child: Text(
+                              "로그인",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 20),
+                            ),
+                            onPressed: () {
+                              print('로그인');
+                              // 로그인 성공시 HomePage로 이동
+                              // Navigator.pushReplacement(
+                              //   context,
+                              //   MaterialPageRoute(builder: (_) => HomePage()),
+                              // );
+                            },
+                          ),
+                        ),
+
+                        SizedBox(
+                          width: double.infinity, // <-- match_parent
+                          child: ElevatedButton(
+                            style: ButtonStyle(
+                                shape: MaterialStateProperty.all<
+                                        RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                            ))),
+
+                            /// 회원가입 버튼
+                            child: Text(
+                              "회원가입",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 20),
+                            ),
+                            onPressed: () {
+                              // 회원가입
+                              print('회원가입');
+                              authService.signUp(
+                                email: emailController.text,
+                                password: passwordController.text,
+                                onSuccess: () {
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(SnackBar(
+                                    content: Text("회원가입 성공"),
+                                  ));
+                                },
+                                onError: (err) {
+                                  // 에러 발생
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(SnackBar(
+                                    content: Text(err),
+                                  ));
+                                },
+                              );
+                            },
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
